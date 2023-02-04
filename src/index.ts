9a64edd897b7dd09f379f64a5588fff138e64c93
LuiiVasquez <luivasque95@gmail.com>
2023-02-03 18:32:57 -0600

    changing project structure - project in OOP structure

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,55 @@
 import "dotenv/config";
-import app from "./app";
 import db from "./config/mongo";
 
-const port = process.env.PORT || 3000;
+import express, { Application } from "express";
+import morgan from "morgan";
+import cors from "cors";
 
-// check connection
-db().then(() => {
-  console.log("mongodb connectd");
-});
+import productsRoutes from "./v1/routes/products.routes";
+import indexRoutes from "./v1/routes/index";
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+class Server {
+  //* server class
+
+  public app: Application; //* app creation with express Application
+
+  constructor() {
+    this.app = express();
+
+    //* ? app config
+    this.config();
+    this.routes();
+    this.database();
+  }
+
+  //* global config method
+  config(): void {
+    this.app.set("port", process.env.PORT || 3000); //* node process
+    this.app.use(morgan("dev"));
+    this.app.use(cors());
+    this.app.use(express.json());
+  }
+
+  //* routes config method
+  routes(): void {
+    this.app.use("/", indexRoutes);
+    this.app.use("/api/v1/products", productsRoutes);
+  }
+
+  //* database config method
+  database(): void {
+    db().then(() => {
+      console.log("mongodb atlas connected");
+    });
+  }
+
+  //* start method, stay listening
+  start(): void {
+    this.app.listen(this.app.get("port"), () => {
+      console.log(`Server listening on por: `, this.app.get("port"));
+    });
+  }
+}
+
+export const server = new Server(); //* constructor start
+server.start();
