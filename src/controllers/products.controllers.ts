a77fced60edd012d38cb43149715de35c2031e82
LuiiVasquez <luivasque95@gmail.com>
2023-02-03 18:31:43 -0600

    turning functional controllers to classes

--- src/controllers/products.controllers.ts
+++ src/controllers/products.controllers.ts
@@ -1,23 +1,28 @@
 import { Request, Response } from "express";
+import { productsServices } from "../services/products.services";
 import { response } from "../common/response";
-import { createProducts, getProducts } from "../services/products.services";
 
-export const getItems = async (req: Request, res: Response) => {
-  try {
-    const result = await getProducts();
+class ItemsController {
+  public getItems = async (req: Request, res: Response) => {
+    try {
+      const result = await productsServices.getProducts();
 
-    response.success(res, 200, "Products list", result);
-  } catch (error) {
-    response.error(res, error);
-  }
-};
+      response.success(res, 200, "Products list", result);
+    } catch (error: any) {
+      response.error(res, error);
+    }
+  };
+  public createItem = async ({ body }: Request, res: Response) => {
+    try {
+      const product = body;
 
-export const createItems = async ({ body }: Request, res: Response) => {
-  try {
-    const result = await createProducts(body);
+      const result = await productsServices.createProducts(product);
 
-    response.success(res, 200, "Product created", result);
-  } catch (error) {
-    response.error(res, error);
-  }
-};
+      response.success(res, 201, "Product created", result);
+    } catch (error: any) {
+      response.error(res, error);
+    }
+  };
+}
+
+export const itemsController = new ItemsController();
