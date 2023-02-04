103ee88fecaeff82d20f540d776748321420aa9b
LuiiVasquez <luivasque95@gmail.com>
2023-02-03 18:31:10 -0600

    turning functions to classes for OOP

--- src/services/products.services.ts
+++ src/services/products.services.ts
@@ -1,19 +1,29 @@
 import ProductModel from "../models/products.model";
 
-export const getProducts = async () => {
-  try {
-    const reponseItems = await ProductModel.find({});
-    return reponseItems;
-  } catch (error: any) {
-    console.log(error.message);
-  }
-};
+class ProductsServices {
+  public response: any; //* public variable for response
 
-export const createProducts = async (item: any) => {
-  try {
-    const reponseItems = await ProductModel.create(item);
-    return reponseItems;
-  } catch (error: any) {
-    console.log(error.message);
+  //* function to get all items
+  public async getProducts() {
+    try {
+      this.response = await ProductModel.find({});
+
+      return this.response;
+    } catch (error: any) {
+      console.log(error.message);
+    }
   }
-};
+
+  //* function to create an item
+  public async createProducts(product: any) {
+    try {
+      this.response = await ProductModel.create(product);
+
+      return this.response;
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+}
+
+export const productsServices = new ProductsServices();
