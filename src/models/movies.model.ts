0ed6cf197e8e4f07812bd1e6b2f1718bc648e465
LuiiVasquez <luivasque95@gmail.com>
2023-02-08 01:34:50 -0600

    creating model for movies

---
+++ src/models/movies.model.ts
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+import { Movie } from "../interfaces/movies.interface";
+
+// * movie schema based on movie interface
+
+const MoviesSchema = new Schema<Movie>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    poster: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    schedules: [{ schedule: String, date: Date }],
+    cinemas: [String],
+  },
+  { timestamps: true, versionKey: false }
+);
+
+//* model creation
+const MovieModel = model("movies", MoviesSchema);
+export default MovieModel;
