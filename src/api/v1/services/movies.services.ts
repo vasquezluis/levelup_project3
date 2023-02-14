8b8225323d225ea028a070fbf99617da39070397
LuiiVasquez <luivasque95@gmail.com>
2023-02-13 22:32:11 -0600

    added getMovie service

--- src/api/v1/services/movies.services.ts
+++ src/api/v1/services/movies.services.ts
@@ -3,6 +3,17 @@ import MovieModel from "../models/movies.model";
 class MoviesServices {
   public response: any; //* public variable for response
 
+  //* function to get one item
+  public async getMovie(name: string) {
+    try {
+      this.response = await MovieModel.findOne({ name });
+
+      return this.response;
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
   //* function to get all items
   public async getMovies() {
     try {
