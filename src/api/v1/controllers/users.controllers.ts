dec10b2f5e43258875ff4ac01522b44c292b5de7
LuiiVasquez <luivasque95@gmail.com>
2023-02-16 12:30:30 -0600

    added get single, active, update, delete controllers

--- src/api/v1/controllers/users.controllers.ts
+++ src/api/v1/controllers/users.controllers.ts
@@ -1,8 +1,29 @@
 import { Request, Response } from "express";
 import { usersServices } from "../services/users.services";
 import { response } from "../../../common/response";
+import createHttpError from "http-errors";
 
 class ItemsController {
+  // * get one item
+  public getItem = async (req: Request, res: Response) => {
+    try {
+      const {
+        params: { id },
+      } = req;
+
+      const result = await usersServices.getUser(id);
+
+      if (!result) {
+        response.error(res, new createHttpError.NotFound("User not found!"));
+      } else {
+        response.success(res, 200, `User ${id}`, result);
+      }
+    } catch (error: any) {
+      response.error(res, error);
+    }
+  };
+
+  // * get all items
   public getItems = async (req: Request, res: Response) => {
     try {
       const result = await usersServices.getUsers();
@@ -13,10 +34,21 @@ class ItemsController {
     }
   };
 
+  // * get all active items
+  public getActiveItems = async (req: Request, res: Response) => {
+    try {
+      const result = await usersServices.getActiveUsers();
+
+      response.success(res, 200, "Active users list", result);
+    } catch (error: any) {
+      response.error(res, error);
+    }
+  };
+
+  // * create an item
   public createItem = async ({ body }: Request, res: Response) => {
     try {
       const user = body;
-      console.log(user);
 
       const result = await usersServices.createUser(user);
 
@@ -26,16 +58,70 @@ class ItemsController {
     }
   };
 
+  // * update an item
+  public updateItem = async (req: Request, res: Response) => {
+    try {
+      const {
+        params: { id },
+      } = req;
+      const body = req.body;
+
+      const result = await usersServices.updateUser(id, body);
+
+      if (!result) {
+        response.error(res, new createHttpError.NotFound("User not found!"));
+      } else {
+        response.success(res, 200, "User updated", result);
+      }
+    } catch (error: any) {
+      response.error(res, error);
+    }
+  };
+
+  // * delete an item
+  public deleteItem = async (req: Request, res: Response) => {
+    try {
+      const {
+        params: { id },
+      } = req;
+
+      const result = await usersServices.deleteUser(id);
+
+      if (!result) {
+        response.error(res, new createHttpError.NotFound("User not found!"));
+      } else {
+        response.success(res, 204, "User deleted", result);
+      }
+    } catch (error: any) {
+      response.error(res, error);
+    }
+  };
+
+  // * get all reservation of a user
   public getUsersReservations = async (req: Request, res: Response) => {
     try {
       const {
-        params: { userId },
+        params: { id },
       } = req;
 
-      const result = await usersServices.getUserReservations(userId);
-      console.log(result)
+      const result = await usersServices.getUserReservations(id);
 
-      response.success(res, 200, `Reservations for user ${userId}`, result);
+      response.success(res, 200, `Reservations for user ${id}`, result);
+    } catch (error) {
+      response.error(res, error);
+    }
+  };
+
+  // * get all accreditations petition of a user
+  public getUsersAccreditations = async (req: Request, res: Response) => {
+    try {
+      const {
+        params: { id },
+      } = req;
+
+      const result = await usersServices.getUserAccreditations(id);
+
+      response.success(res, 200, `Accreditations for user ${id}`, result);
     } catch (error) {
       response.error(res, error);
     }
