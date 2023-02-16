2c7037134bf289d8ad2c1a8c4b77cbe1c1897035
LuiiVasquez <luivasque95@gmail.com>
2023-02-16 12:31:09 -0600

    added get single, active, update, delete controllers

--- src/api/v1/controllers/movies.controllers.ts
+++ src/api/v1/controllers/movies.controllers.ts
@@ -1,21 +1,29 @@
 import { Request, Response } from "express";
 import { moviesServices } from "../services/movies.services";
 import { response } from "../../../common/response";
+import createHttpError from "http-errors";
 
 class ItemsController {
+  // * get one item
   public getItem = async (req: Request, res: Response) => {
     try {
       const {
-        params: { movieName },
+        params: { id },
       } = req;
 
-      const result = await moviesServices.getMovie(movieName);
+      const result = await moviesServices.getMovie(id);
 
-      response.success(res, 200, `Movie data for ${movieName}`, result);
+      if (!result) {
+        response.error(res, new createHttpError.NotFound("Movie not found!"));
+      } else {
+        response.success(res, 200, `Movie data for ${id}`, result);
+      }
     } catch (error: any) {
       response.error(res, error);
     }
   };
+
+  // * get all items
   public getItems = async (req: Request, res: Response) => {
     try {
       const result = await moviesServices.getMovies();
@@ -25,14 +33,65 @@ class ItemsController {
       response.error(res, error);
     }
   };
+
+  // * get all active items
+  public getActiveItems = async (req: Request, res: Response) => {
+    try {
+      const result = await moviesServices.getActiveMovies();
+
+      response.success(res, 200, "Active movies list", result);
+    } catch (error: any) {
+      response.error(res, error);
+    }
+  };
+
+  // * create an item
   public createItem = async ({ body }: Request, res: Response) => {
     try {
       const movie = body;
-      console.log(movie);
 
       const result = await moviesServices.createMovie(movie);
 
       response.success(res, 201, "Movie created", result);
+    } catch (error: any) {
+      response.error(res, error);
+    }
+  };
+
+  // * update an item
+  public updateItem = async (req: Request, res: Response) => {
+    try {
+      const {
+        params: { id },
+      } = req;
+      const body = req.body;
+
+      const result = await moviesServices.updateMovie(id, body);
+
+      if (!result) {
+        response.error(res, new createHttpError.NotFound("Movie not found!"));
+      } else {
+        response.success(res, 200, "Movie updated", result);
+      }
+    } catch (error: any) {
+      response.error(res, error);
+    }
+  };
+
+  // * delete an item
+  public deleteItem = async (req: Request, res: Response) => {
+    try {
+      const {
+        params: { id },
+      } = req;
+
+      const result = await moviesServices.deleteMovie(id);
+
+      if (!result) {
+        response.error(res, new createHttpError.NotFound("Movie not found!"));
+      } else {
+        response.success(res, 204, "Movie deleted", result);
+      }
     } catch (error: any) {
       response.error(res, error);
     }
