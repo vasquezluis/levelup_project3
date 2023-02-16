a92a3dfe6995887380731ddd9092601634c97d22
LuiiVasquez <luivasque95@gmail.com>
2023-02-16 12:23:05 -0600

    added update, delete, getReservations & getAccreditations services

--- src/api/v1/services/users.services.ts
+++ src/api/v1/services/users.services.ts
@@ -1,10 +1,22 @@
 import UserModel from "../models/users.model";
 import ReservationModel from "../models/reservations.model";
+import AccreditationModel from "../models/accreditations.model";
 
 class UsersServices {
-  public response: any; //* public variable for response
+  public response: any; // * public variable for response
 
-  //* function to get all items
+  // * function to get one item
+  public async getUser(id: string) {
+    try {
+      this.response = await UserModel.findById(id);
+
+      return this.response;
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  // * function to get all items
   public async getUsers() {
     try {
       this.response = await UserModel.find({});
@@ -15,7 +27,18 @@ class UsersServices {
     }
   }
 
-  //* function to create an item
+  // * function to get all active items
+  public async getActiveUsers() {
+    try {
+      this.response = await UserModel.find({ active: true });
+
+      return this.response;
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  // * function to create an item
   public async createUser(user: any) {
     try {
       this.response = await UserModel.create(user);
@@ -26,10 +49,48 @@ class UsersServices {
     }
   }
 
-  //* function to get all items for a certain user
-  public async getUserReservations(userId: string) {
+  // * function to update an item
+  public async updateUser(id: string, body: any) {
     try {
-      this.response = await ReservationModel.find({ userId });
+      this.response = await UserModel.findByIdAndUpdate({ _id: id }, body, {
+        new: true,
+      });
+
+      return this.response;
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  // * function to soft delete an item
+  public async deleteUser(id: string) {
+    try {
+      this.response = await UserModel.findByIdAndUpdate(
+        { _id: id },
+        { $set: { active: false } }
+      );
+
+      return this.response;
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  // * function to get all reservations for a certain user
+  public async getUserReservations(id: string) {
+    try {
+      this.response = await ReservationModel.find({ userId: id });
+
+      return this.response;
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  }
+
+  // * function to get all accreditations for a certain user
+  public async getUserAccreditations(id: string) {
+    try {
+      this.response = await AccreditationModel.find({ userId: id });
 
       return this.response;
     } catch (error: any) {
