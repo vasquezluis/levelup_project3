d58a9eb6ca25f27037797f546f51f6939fe54478
LuiiVasquez <luivasque95@gmail.com>
2023-02-16 12:16:27 -0600

    created model for accreditations function

---
+++ src/api/v1/models/accreditations.model.ts
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose";
+import { Accreditation } from "../interfaces/accreditation.interface";
+
+// * accreditation schema based on accreditation interface
+
+const AccreditationSchema = new Schema<Accreditation>(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    credits: {
+      type: Number,
+      required: true,
+    },
+    coupon: {
+      type: String,
+      default: null,
+    },
+    paid: {
+      type: Boolean,
+    },
+    accepted: {
+      type: Boolean,
+      default: false,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+//* model creation
+const AccreditationModel = model("accreditations", AccreditationSchema);
+export default AccreditationModel;
