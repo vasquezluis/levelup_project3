47adf16cc204350da45cd9631d4af86e1e961cef
LuiiVasquez <luivasque95@gmail.com>
2023-02-22 00:22:21 -0600

    added trailer to interface

--- src/api/v1/interfaces/movies.interface.ts
+++ src/api/v1/interfaces/movies.interface.ts
@@ -5,9 +5,10 @@
 export interface Movie {
   name: string;
   poster: string;
+  trailer: string;
   genders: string[];
   cost: number;
   description: string;
   schedules: any[];
-  active: boolean 
+  active: boolean;
 }
