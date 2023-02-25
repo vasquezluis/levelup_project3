a0979ada32615a5dc62aebe22c015fd603975f9f
LuiiVasquez <luivasque95@gmail.com>
2023-02-25 14:04:59 -0600

    added authentication using roles -> user

---
+++ src/api/v1/middlewares/userAuthValidator.middlewares.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+
+export const requireAuth = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const secret: any = process.env.JWT_SECRET;
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) return res.status(401).json({ message: "Unauthorized" });
+
+  const token = authHeader.split(" ")[1];
+
+  if (!token) return res.status(401).json({ message: "Unauthorized" });
+
+  jwt.verify(token, secret, (error: any, payload: any) => {
+    if (error) return res.json({ message: error });
+
+    if (payload.roles.includes("user") || payload.roles.includes("admin")) {
+      next();
+    } else {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+  });
+};
