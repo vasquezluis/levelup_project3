6a9d7e3f769de3cf8c67744b2f00dfbb8a514d83
LuiiVasquez <luivasque95@gmail.com>
2023-02-25 14:10:37 -0600

    added token validator to routes

--- src/api/v1/routes/reservations.routes.ts
+++ src/api/v1/routes/reservations.routes.ts
@@ -1,5 +1,10 @@
 import { Router } from "express";
 import { itemsController } from "../controllers/reservations.controllers";
+
+// ? authtentications
+import { requireAdminAuth } from "../middlewares/adminAuthValidator.middlewares";
+import { requireAuth } from "../middlewares/userAuthValidator.middlewares";
+
 import { schemaValidation } from "../middlewares/schemaValidator.middlewares";
 import { CreateMoviesSchema } from "../schemas/movies.schema";
 
@@ -16,12 +21,16 @@ class ReservationsRoutes {
 
   //* routes config
   config(): void {
-    this.router.get("/active", itemsController.getActiveItems);
-    this.router.get("/:id", itemsController.getItem);
-    this.router.get("/", itemsController.getItems);
-    this.router.post("/", itemsController.createItem);
-    this.router.put("/:id", itemsController.updateItem);
-    this.router.delete("/:id", itemsController.deleteItem);
+    this.router.get(
+      "/active",
+      requireAdminAuth,
+      itemsController.getActiveItems
+    );
+    this.router.get("/:id", requireAdminAuth, itemsController.getItem);
+    this.router.get("/", requireAdminAuth, itemsController.getItems);
+    this.router.post("/", requireAuth, itemsController.createItem);
+    this.router.put("/:id", requireAdminAuth, itemsController.updateItem);
+    this.router.delete("/:id", requireAdminAuth, itemsController.deleteItem);
   }
 }
 
